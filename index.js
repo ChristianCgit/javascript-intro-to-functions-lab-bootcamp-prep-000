30ce32b7178bf88a5c3464e222faa8d57b88225a
christian.cook91@outlook.com <christian.cook91@outlook.com>
2018-07-12 12:54:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,14 +17,14 @@ function sayHiToGrandma(string){
   if(sayHiToGrandma === whisper.toLowerCase);{
   return(lowercase)}
 }
-function sayHiToGrandma(string){  
+
   var uppercase = "YES INDEED!"
   if (sayHiToGrandma === shout.toUpperCase);{
   return(uppercase)}
-}
- function sayHiToGrandma(string){ 
+
+
    var mixedcase = "I love you, too!"
   if(sayHiToGrandma === "I love you  Grandma");{
   return(mixedcase)
-}
+
 }
